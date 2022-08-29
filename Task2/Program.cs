f47e42638d1ad297ab479de5c84fb69135a58931
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-30 01:40:02 +0600

    Seminar 4

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N");
+int number = int.Parse(Console.ReadLine());
+int Factorial(int number)
+{
+    int count = 1;
+    int i = 1;
+    while (i<=number)
+    {
+        count = count*i;
+        i = i + 1;
+    }
+    return count;
+}
+
+int result = Factorial(number);
+Console.WriteLine(result);
