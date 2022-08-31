44b6678162cf53e7f15a8dfcbd60fb61f5a753f8
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-08-31 16:00:00 +0600

    Tasks 1,2

---
+++ Seminar2_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+int a1 = number/100;
+int a3 = number%10;
+
+Console.WriteLine($"{a1}{a3}");
+
+//Вариант решения через массив
+//int number = new Random().Next(100, 100);
+//string number1 = Convert.ToString(number);
+//Console.WriteLine(number1);
+//Console.WriteLine(number1[0]+""+number1[2]);
