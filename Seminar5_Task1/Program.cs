e0154ee61c52634c9db621e9c2902ede6106feec
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-02 00:22:50 +0600

    Seminar 5

---
+++ Seminar5_Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+
+int[]array = new int [12];
+
+for(int i=0; i<12; i++)
+{
+    array[i] = new Random().Next(-9,10);
+}
+
+int positiveSum = 0;
+int negativeSum = 0;
+foreach (int value in array)
+{
+    positiveSum += value > 0 ? value : 0;
+    negativeSum += value < 0 ? value : 0;
+}
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Positive sum = {positiveSum}, Negative sum = {negativeSum}");
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int pos = 0; pos < count; pos++)
+    {
+        Console.Write(col[pos] + " ");
+    }
+}
---
+++ Seminar5_Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив. 
+//Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int[]array = new int [12];
+
+for(int i=0; i<12; i++)
+{
+    array[i] = new Random().Next(-9,10);
+}
+
+/*void search(int[] array_num, int num)
+{
+    for (int i = 0; i < array_num.Length; i++)
+    {
+        if (array_num[i] == num)
+        {
+            Console.WriteLine("Число есть");
+            return;
+        }
+    }
+    Console.WriteLine("Числа нет");
+}
+
+search(array, 3);*/
+
+/*Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+void SunNumber(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine(sum);
+}
+SunNumber(number);*/
+
