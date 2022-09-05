66be544eb116c92d75c2b61236ce6df66e182d88
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-05 15:39:33 +0600

    Seminar 5 Task 4

--- Seminar5_Task4/Program.cs
+++ Seminar5_Task4/Program.cs
@@ -1,10 +1,41 @@
 ﻿// Задайте одномерный массив из 123 случайных чисел. 
 //Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 
-int[]array = new int [123];
-
-for(int i=0; i<12; i++)
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    array[i] = new Random().Next(-1000,1001);
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+    res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
 }
 
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for(int pos = 0; pos < count; pos++)
+    {
+        Console.Write(col[pos] + " ");
+    }
+}
+
+int[]array = GetArray(123,-100,100);
+PrintArray(array);
+Console.WriteLine();
+
+int Count(int[] array, int minLength, int maxLength)
+{
+    int count = 0;
+    foreach(int value in array)
+    {
+        if(value>=minLength && value<=maxLength)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+int res = Count(array, 10,99);
+Console.WriteLine(res);
