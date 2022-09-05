0c10e4fdf6d5c404d3713059010eb10ebc2c332b
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-06 00:21:33 +0600

    Seminar 6

---
+++ Seminar6_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+    res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+
+void ReverseArray(int[]inArray)
+{
+    for (int i = 0; i < inArray.Length/2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length-i-1];
+        inArray[inArray.Length-i-1] = k;
+    }
+}
+
+int[]array = GetArray(10,0,10);
+Console.WriteLine(String.Join(" ", array));
+
+ReverseArray(array);
+Console.WriteLine(String.Join(" ", array));
+
+
+
---
+++ Seminar6_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+void Binary(int num)
+{
+    string binar = "";
+    while(num !=0)
+    {
+        binar = num % 2 + binar;
+        num = num/2;
+    }
+    Console.WriteLine(binar);
+}
+
+Binary(num);
