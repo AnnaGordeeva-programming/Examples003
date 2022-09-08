c41d7730fd72b726e94bb17f78f4b517f2be2a21
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-08 09:24:48 +0600

    Seminar6 Task 4

---
+++ Seminar6_Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+Console.WriteLine("Введите число 1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+int num3 = int.Parse(Console.ReadLine());
+
+void Triangle(int num1, int num2, int num3)
+{
+    if(num1+num2<=num3 || num1+num3<=num2 || num2+num3<=num1) 
+    {
+        Console.WriteLine("Треуголник НЕ получится");
+    }
+    else Console.WriteLine("Треуголник получится");
+}
+
+Triangle(num1, num2, num3);
