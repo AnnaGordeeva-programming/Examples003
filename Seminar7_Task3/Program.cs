778fdbd14737c7aa84f960fb45c63360631c8659
Anna Gordeeva <gordeeva.anet@gmail.com>
2022-09-12 22:35:44 +0600

    Task 3,4

---
+++ Seminar7_Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SquareElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i = i + 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j = j + 2)
+        {
+            array[i, j] *= array[i,j];
+        }
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+SquareElements(array);
+PrintArray(array);
---
+++ Seminar7_Task4/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив. 
+//Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int DiagonalSum(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += array[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine(DiagonalSum(array));
